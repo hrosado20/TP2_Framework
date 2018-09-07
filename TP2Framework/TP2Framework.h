1cbdc774a2f90936132b1e08af14deedd8bce144
hrool20 <hugo_rosol@hotmail.com>
2018-09-07 10:55:57 -0500

    Initial Commit

---
+++ TP2Framework/TP2Framework.h
@@ -0,0 +1,19 @@
+//
+//  TP2Framework.h
+//  TP2Framework
+//
+//  Created by Hugo Andres on 7/09/18.
+//  Copyright © 2018 UPC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TP2Framework.
+FOUNDATION_EXPORT double TP2FrameworkVersionNumber;
+
+//! Project version string for TP2Framework.
+FOUNDATION_EXPORT const unsigned char TP2FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TP2Framework/PublicHeader.h>
+
+
---
+++ TP2FrameworkTests/TP2FrameworkTests.swift
@@ -0,0 +1,36 @@
+//
+//  TP2FrameworkTests.swift
+//  TP2FrameworkTests
+//
+//  Created by Hugo Andres on 7/09/18.
+//  Copyright © 2018 UPC. All rights reserved.
+//
+
+import XCTest
+@testable import TP2Framework
+
+class TP2FrameworkTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
